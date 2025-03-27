06f1e8fcc0f1c68ecdc51e81952c22009558be4c
Anurag <dev.anuragkr@gmail.com>
2025-03-27 22:58:42 +0530

    test

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,22 @@
-const HomePage = () => {
+import { Suspense } from 'react';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { ClientGreeting } from './client-greating';
+
+const HomePage = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: 'world' }));
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ClientGreeting />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrationBoundary>
   );
 };
 
