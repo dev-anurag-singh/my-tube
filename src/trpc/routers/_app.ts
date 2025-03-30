64f72127dabc4eedc1759feb630ea37c29def70e
Anurag <dev.anuragkr@gmail.com>
2025-03-30 23:30:29 +0530

    replace baseProcedure to protectedProcedute

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,14 +1,16 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
 // import { TRPCError } from '@trpc/server';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       })
     )
     .query(opts => {
+      console.log({ user: opts.ctx.user });
+
       return {
         greeting: `hello ${opts.input.text}`,
       };
